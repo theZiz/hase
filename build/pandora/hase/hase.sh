533ae1b62c3b6498a8b3623ec88d7f35d3c827d7
Ziz <zizsdl@googlemail.com>
2013-11-20 13:58:51 +0100

    weapon

--- build/pandora/hase/hase.sh
+++ build/pandora/hase/hase.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 export PATH=":${PATH:-"/usr/bin:/bin:/usr/local/bin"}"
 export LD_LIBRARY_PATH=":${LD_LIBRARY_PATH:-"/usr/lib:/lib"}"
-export HOME="/mnt/utmp/puzzletube" XDG_CONFIG_HOME="/mnt/utmp/puzzletube"
+export HOME="/mnt/utmp/hase" XDG_CONFIG_HOME="/mnt/utmp/hase"
 
-if [ -d /mnt/utmp/puzzletube/share ];then
-	export XDG_DATA_DIRS=/mnt/utmp/puzzletube/share:$XDG_DATA_DIRS:/usr/share
+if [ -d /mnt/utmp/hase/share ];then
+	export XDG_DATA_DIRS=/mnt/utmp/hase/share:$XDG_DATA_DIRS:/usr/share
 fi
 export SDL_AUDIODRIVER="alsa"
 
-cat /proc/pandora/nub0/mode > /tmp/nub0mode_before_puzzletube
-cat /proc/pandora/nub1/mode > /tmp/nub1mode_before_puzzletube
+cat /proc/pandora/nub0/mode > /tmp/nub0mode_before_hase
+cat /proc/pandora/nub1/mode > /tmp/nub1mode_before_hase
 echo absolute > /proc/pandora/nub0/mode
 echo absolute > /proc/pandora/nub1/mode
 
-LD_LIBRARY_PATH=. ./puzzletube $*
+LD_LIBRARY_PATH=. ./hase $*
 
-cat /tmp/nub0mode_before_puzzletube > /proc/pandora/nub0/mode
-cat /tmp/nub1mode_before_puzzletube > /proc/pandora/nub1/mode
+cat /tmp/nub0mode_before_hase > /proc/pandora/nub0/mode
+cat /tmp/nub1mode_before_hase > /proc/pandora/nub1/mode
  
-rm /tmp/nub0mode_before_puzzletube /tmp/nub1mode_before_puzzletube
+rm /tmp/nub0mode_before_hase /tmp/nub1mode_before_hase
