ba30cf2e0d5c47860b844ba4a3370bebe28e2f44
Ziz <zizsdl@googlemail.com>
2014-10-02 00:24:47 +0200

    Added symlink php script creation to have always up2date links (I don't have to edit EVERY post about hase, I just need to link to hase-TARGET.zip). ;)

--- make_release_packages.sh
+++ make_release_packages.sh
@@ -9,6 +9,7 @@ echo "<body>" >> index.htm
 TIME=`date -u +"%d.%m.%Y %R"`
 echo "Updated at the $TIME." >> index.htm
 echo "<h1>$PROGRAM download links:</h1>" >> index.htm
+echo "<?php" > symlink.php
 for f in $DEST
 do
 	if [ -e "$f/$PROGRAM/$PROGRAM" ] || [ -e "$f/$PROGRAM/$PROGRAM.exe" ]; then
@@ -31,6 +32,8 @@ do
 				tar cfvz "$PROGRAM-$NAME-$VERSION.tar.gz" * > /dev/null
 				mv "$PROGRAM-$NAME-$VERSION.tar.gz" ../..
 				echo "<a href=$PROGRAM-$NAME-$VERSION.tar.gz>$NAME</a></br>" >> ../../index.htm
+				echo "unlink('$PROGRAM-$NAME.tar.gz');" >> ../../symlink.php
+				echo "symlink('$PROGRAM-$NAME-$VERSION.tar.gz', '$PROGRAM-$NAME.tar.gz');" >> ../../symlink.php
 			else
 				#no Pandora, no PC. So we have a low resolution device!
 				if [ $NAME != "win32" ]; then
@@ -44,6 +47,8 @@ do
 					zip -r "$PROGRAM-$NAME-$VERSION.zip" * > /dev/null
 					mv "$PROGRAM-$NAME-$VERSION.zip" ../..
 					echo "<a href=$PROGRAM-$NAME-$VERSION.zip>$NAME</a></br>" >> ../../index.htm
+					echo "unlink('$PROGRAM-$NAME.zip');" >> ../../symlink.php
+					echo "symlink('$PROGRAM-$NAME-$VERSION.zip', '$PROGRAM-$NAME.zip');" >> ../../symlink.php
 				fi
 			fi
 		fi
@@ -56,5 +61,6 @@ do
 		cd ..
 	fi
 done
+echo "?>" >> symlink.php
 echo "</body>" >> index.htm
 echo "</html>" >> index.htm
