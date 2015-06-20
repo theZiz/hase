5e164f78001356596346fc1ea9ede3122ccf9345
Alexander <ziz@mailbox.org>
2015-06-20 16:17:11 +0200

    Added desktop files for hase

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 if [ $# -gt 1 ]
 then
 	make clean
-	make PARAMETER="-DDATA_FOLDER=\"\\\"$2/hase\\\"\""
+	make PARAMETER="-DDATA_FOLDER=\"\\\"$2/hase\\\"\"" NO_DEBUG=1
 	cp hase "$1/hase"
 	if [ ! -d "$2/hase" ]; then mkdir "$2/hase";fi
 	cp -r data "$2/hase"
@@ -10,6 +10,8 @@ then
 	cp -r sprites "$2/hase"
 	cp -r sounds "$2/hase"
 	cp hase_readme.txt "$2/hase"
+	cp hase.png "$2/pixmaps"
+	cp hase.desktop "$2/applications"
 	make clean
 else
 	echo "Usage: (sudo) ./install.sh <bin directory> <share directory>"
--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,8 @@ if [ $# -gt 1 ]
 then
 	rm -f "$1/hase"
 	rm -rf "$2/hase"
+	rm -f "$2/pixmaps/hase.png"
+	rm -f "$2/applications/hase.desktop"
 else
 	echo "Usage: (sudo) ./uninstall.sh <bin directory> <share directory>"
 	echo "e.g:"
