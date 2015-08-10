92a7702fccb7ca25ac7ae5c3bf801040bb9f381d
Ziz <zizsdl@googlemail.com>
2015-08-10 14:49:05 +0200

    Asking for saving level

--- lobbyGame.h
+++ lobbyGame.h
@@ -6,6 +6,7 @@
 #include "client.h"
 
 int game_options(Uint32 *game_opt,int* game_seconds,int* game_hares,spFontPointer font, void ( *resize )( Uint16 w, Uint16 h ));
+void save_level(char* level_string);
 void start_lobby_game(spFontPointer font, void ( *resize )( Uint16 w, Uint16 h ),pGame game,int spectate);
 int lg_reload(void* dummy);
 
